999d2f8db2cd56613924a4371d8e9c0ff2cf7760
wasiq abdullah <chwasiq0569>
2023-02-16 23:48:50 +0500

    Added get todos functionality

--- todos/urls.py
+++ todos/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import list_todos
+from .views import list_todos, insert_todo_item
 
 urlpatterns = [
-    path('list/', list_todos),
+    path('list/', list_todos.as_view()),
+    path('insert_todo/', insert_todo_item),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
 # Create your views here.
+from .models import Todo
+from rest_framework.response import Response
+from .serializer import TodoSerializer
+from rest_framework.views import APIView
 
 
-def list_todos(request):
-    return HttpResponse("HELLO WORLD")
+class list_todos(APIView):
+    def get(self, request, format=None):
+        todos = Todo.objects.all()
+        print(todos)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data)
+
+
+def insert_todo_item(request: HttpRequest):
+    content = request.POST['content']
+    todo = Todo(content)
+    todo.save()
+    return HttpResponse(todo)
